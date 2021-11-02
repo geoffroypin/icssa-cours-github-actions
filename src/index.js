847d0ee6de15dc08ea22ca4489d4c736234edc7c
Geoffroy PIN <geoffroy.pin@ensemblescolaire-niort.com>
2021-11-02 15:21:02 +0100

    Prettier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 export default class Pencil {
   constructor(durability = 50, length = 50, eraserDurability) {
-    this.durability = durability
+    this.durability = durability;
     this.maxDurability = durability;
     this.length = length;
-    this.eraserDurability = eraserDurability
+    this.eraserDurability = eraserDurability;
   }
 
   getPencilDurability() {
@@ -11,7 +11,7 @@ export default class Pencil {
   }
 
   getPencilLength() {
-    return this.length
+    return this.length;
   }
 
   getEraserDurability() {
@@ -20,7 +20,9 @@ export default class Pencil {
 
   updatePencilDurability(character) {
     if (character !== " ") {
-      character === character.toLowerCase() ? this.durability -= 1 : this.durability -= 2;
+      character === character.toLowerCase()
+        ? (this.durability -= 1)
+        : (this.durability -= 2);
     }
   }
 
@@ -31,7 +33,7 @@ export default class Pencil {
   writeOnPaper(paper, textToWrite) {
     for (let i = 0; i < textToWrite.length; i++) {
       this.updatePencilDurability(textToWrite.charAt(i));
-      this.durability >= 0 ? paper += textToWrite.charAt(i) : paper += " ";
+      this.durability >= 0 ? (paper += textToWrite.charAt(i)) : (paper += " ");
     }
 
     return paper;
@@ -49,7 +51,7 @@ export default class Pencil {
       return;
     }
 
-    var charactersOnPaper = paper.split('');
+    var charactersOnPaper = paper.split("");
     const indexOfWord = paper.lastIndexOf(text) + text.length - 1;
 
     for (let i = 0; i < text.length; i++) {
@@ -59,7 +61,7 @@ export default class Pencil {
       charactersOnPaper[indexOfWord - i] = " ";
     }
 
-    return charactersOnPaper.join('');
+    return charactersOnPaper.join("");
   }
 
   edit(paper, textToAdd) {
@@ -67,7 +69,7 @@ export default class Pencil {
       return;
     }
 
-    const charactersOnPaper = paper.split('');
+    const charactersOnPaper = paper.split("");
     const indexOfBlankSpace = paper.indexOf("  ") + 1;
 
     for (let i = 0; i < textToAdd.length; i++) {
@@ -78,6 +80,6 @@ export default class Pencil {
       }
     }
 
-    return charactersOnPaper.join('');
+    return charactersOnPaper.join("");
   }
-};
+}
